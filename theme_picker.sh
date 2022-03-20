6aa700b801016d495a02f47f105c7903097fd514
kRustyKr3b <sergejjakovljev@outlook.com>
2022-03-20 18:27:32 +0100

    modified:   theme_picker.sh

--- theme_picker.sh
+++ theme_picker.sh
@@ -4,31 +4,31 @@ case "$choice" in
 	OG)
 		wal --theme /home/shaolinrus/wal_themes/og.json
 		cp .config/polybar/colors/og-colors.ini .config/polybar/colors.ini
-		cp ~/wal_themes/prompts/ogPrompt ~/prompt_cols
+		cp ~/wal_themes/prompts/ogPrompt ~/wal_themes/prompt_cols
 		replace "URxvt*background:   [100]#111010" "URxvt*background:   [85]#111010" -- ~/.cache/wal/colors.Xresources
 		notify-send OG;;
 	NEON)
 		wal --theme /home/shaolinrus/wal_themes/neon.json
 		cp .config/polybar/colors/neon-colors.ini .config/polybar/colors.ini
-		cp ~/wal_themes/prompts/neonPrompt ~/prompt_cols
+		cp ~/wal_themes/prompts/neonPrompt ~/wal_themes/prompt_cols
 		replace "URxvt*background:   [100]#111010" "URxvt*background:   [85]#111010" -- .cache/wal/colors.Xresources
 		notify-send NEON;;
 	PUMPKIN)
 		cp .config/polybar/colors/pumpkin-colors.ini .config/polybar/colors.ini
 		wal --theme ~/wal_themes/pumpkin.json
-		cp ~/wal_themes/prompts/pumpkinPrompt ~/prompt_cols
+		cp ~/wal_themes/prompts/pumpkinPrompt ~/wal_themes/prompt_cols
 		replace "URxvt*background:   [100]#111010" "URxvt*background:   [85]#111010" -- .cache/wal/colors.Xresources
 		notify-send PUMPKIN;;
 	NORD)
 		cp .config/polybar/colors/nord-colors.ini .config/polybar/colors.ini
 		wal --theme ~/wal_themes/nord.json
-		cp ~/wal_themes/prompts/nordPrompt ~/prompt_cols
+		cp ~/wal_themes/prompts/nordPrompt ~/wal_themes/prompt_cols
 		replace "URxvt*background:   [100]#111010" "URxvt*background:   [85]#111010" -- .cache/wal/colors.Xresources
 		notify-send -u LOW -t 300 NORD;;
 	HK)
 		cp .config/polybar/colors/hk-colors.ini .config/polybar/colors.ini
 		wal --theme ~/wal_themes/HK.json
-		cp ~/wal_themes/prompts/hkPrompt ~/prompt_cols
+		cp ~/wal_themes/prompts/hkPrompt ~/wal_themes/prompt_cols
 		replace "URxvt*background:   [100]#111010" "URxvt*background:   [85]#111010" -- .cache/wal/colors.Xresources
 		notify-send HK;;
 esac
