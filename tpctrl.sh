5b18b6d01227c0b163b4e282c33f34dbf0516287
kRustyKr3b <sergejjakovljev@outlook.com>
2022-06-16 23:40:06 +0200

    modified:   tpctrl.sh

--- tpctrl.sh
+++ tpctrl.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-state=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
-synclient TouchpadOff="$state"
-[[ "$state" -eq 1 ]] && notify-send "BLOCKED" || notify-send "UNBLOCKED"
+# Toggle touchpad status
+# Using libinput and xinput
+
+# Use xinput list and do a search for touchpads. Then get the first one and get its name.
+device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o | head -n1)"
+
+# If it was activated disable it and if it wasn't disable it
+if [[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]]; then
+		xinput disable "$device";
+		notify-send "DISABLED"
+
+	else
+		xinput enable "$device"
+		notify-send "ENABLED"
+fi
+
