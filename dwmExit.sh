13372ef00b70c2814e4f437f3019012e52ce8147
kRustyKr3b <sergejjakovljev@outlook.com>
2022-09-11 14:29:39 +0200

    modified:   dwmExit.sh

--- dwmExit.sh
+++ dwmExit.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-var=$(echo -e "Suspend\nExit DWM\nReboot\nShutdown" | dmenu -fn 'Iosevka Term-9' -p "Action:")
+var=$(echo -e "Lock\nSuspend\nExit DWM\nReboot\nShutdown" | rofi -dmenu -p "Action" -l 5)
 
 case "$var" in 
+	"Lock") betterlockscreen -l;;
 	"Suspend") systemctl suspend & betterlockscreen -l;;
 	"Exit DWM") killall dwm;;
 #	"Restart DWM") killall dwm && ./home/shaolinrus/scripts/dwmRestart.sh;;
